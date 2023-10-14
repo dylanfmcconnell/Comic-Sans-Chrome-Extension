f3cdfe527c6190c0a959605c3d3dd538e65e46c4
w-o-r-l-d-w-i-d-e <dylanfmcconnell@gmail.com>
2023-10-13 20:38:45 -0400

    Upload

---
+++ content.js
@@ -0,0 +1,12 @@
+let enabled = false;
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "toggle") {
+        enabled = !enabled;
+        if(enabled) {
+            document.body.style.fontFamily = "Comic Sans MS";
+        } else {
+            document.body.style.fontFamily = "initial";
+        }
+    }
+});
